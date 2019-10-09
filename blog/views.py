584dc8707264331769e241a8c46f0dbb7de47fda
Adam-L-Huerta <41498124+Adam-L-Huerta@users.noreply.github.com>
2019-10-09 18:12:51 -0500

    Uploading Project Files

---
+++ blog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+
+
+def index(requests):
+    posts = Post.objects.all()
+    return render(requests, 'index.html', {'posts': posts})
+
+
+def post(requests, slug):
+    posts = Post.objects.all()
+    return render_to_response('post.html', {
+        'post': get_object_or_404(Post, slug=slug),
+        'posts': posts
+    })
+
+
+def about(requests):
+    return render(requests, "about.html", {})
